12c87de3d90e87cd9d715de03f758c1469ce5d63
kawshalyaliyanaarachchi12 <kawshalyaliyanaarachchi12@gmail.com>
2026-08-07 16:53:42 +0530

    Completed auth service setup and endpoints

---
+++ auth-service/src/test/java/com/nexuscart/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexuscart.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
